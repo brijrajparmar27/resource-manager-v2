b06afd155b4c0b0dcf7bb591b98eb83ba5ff2c76
Brijrajsinh parmar <73275164+brijrajparmar27@users.noreply.github.com>
2025-03-21 17:15:57 +0530

    added fonts

--- src/components/bottomDock/BottomDock.tsx
+++ src/components/bottomDock/BottomDock.tsx
@@ -3,19 +3,19 @@ const BottomDock = () => {
     <div className="flex-[1] flex gap-2 p-2 pt-0">
       <div className="glass h-full flex-[1] flex flex-col">
         <div className="flex-[1] max-h-[35px] flex items-center px-2">
-          <p className="text-xl text-blue-400">Ram</p>
+          <p className="text-lg text-blue-400">CPU</p>
         </div>
         <div className="flex-[1]"></div>
       </div>
       <div className="glass h-full flex-[1] flex flex-col">
         <div className="flex-[1] max-h-[35px] flex items-center px-2">
-          <p className="text-xl text-blue-400">Ram</p>
+          <p className="text-lg text-blue-400">RAM</p>
         </div>
         <div className="flex-[1]"></div>
       </div>
       <div className="glass h-full flex-[1] flex flex-col">
         <div className="flex-[1] max-h-[35px] flex items-center px-2">
-          <p className="text-xl text-blue-400">Ram</p>
+          <p className="text-lg font text-blue-400">STORAGE</p>
         </div>
         <div className="flex-[1]"></div>
       </div>
